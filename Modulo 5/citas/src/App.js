e03f3f18bbbf705afacb387794104b2b4c0c1867
MarciaGiselle <marcitoledo@alumno.unlam.edu.ar>
2020-03-24 20:26:40 -0300

    Desarrollo del componente Formulario

--- Modulo 5/citas/src/App.js
+++ Modulo 5/citas/src/App.js
@@ -1,17 +1,24 @@
-import React, {Fragment} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Alert } from 'react-bootstrap';
-import Navegador from './components/Nav';
+import React, { Fragment } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
+import Navegador from "./components/Nav";
+import Formulario from "./components/Formulario";
+
 
 function App() {
   return (
     <Fragment>
-     <Navegador/>      
-    <Alert variant='success my-1'>  
-      <h2 className='text-center'>Administrador de pacientes</h2>
-    </Alert>
-    </Fragment>
+      <Navegador />
+        <h4 className="text-center text-primary my-3">Administrador de pacientes</h4>
+      <Container>
+        <Row>
+            <Col xs={12} md={6}><Formulario/>
 
+            </Col>
+            <Col xs={12} md={6}>2</Col>
+        </Row>
+      </Container>
+    </Fragment>
   );
 }
 
---
+++ Modulo 5/citas/src/components/Formulario.jsx
@@ -0,0 +1,117 @@
+import React, { Fragment, useState } from "react";
+import { Form, Row, Button } from "react-bootstrap";
+import uuid from "uuid/v4";
+
+const Formulario = () => {
+  const [cita, setState] = useState({
+    mascota: "",
+    padre: "",
+    fecha: "",
+    hora: "",
+    sintoma: ""
+  });
+
+  const [error, setError] = useState(false);
+
+  //Funcion que se ejecuta cada vez que el usuario escribe en el input
+  const handleChangeState = e => {
+    setState({
+      ...cita,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  //Extraigo cada uno de los valores de cita
+  const { mascota, padre, fecha, hora, sintoma } = cita;
+
+  //Funcion de Submit del formulario
+  const submitForm = e => {
+    e.preventDefault();
+
+    if (
+      mascota.trim() === "" ||
+      padre.trim() === "" ||
+      fecha.trim() === "" ||
+      hora.trim() === "" ||
+      sintoma.trim() === ""
+    ) {
+      setError(true);
+      return;
+    }
+    setError(false);
+
+    cita.id= uuid();
+    console.log(cita);
+
+
+  };
+
+  //Validar Campos
+
+  return (
+    <Fragment>
+      <h4 className="font-weight-bold">Crear nueva cita</h4>
+      <hr />
+
+      {error ? (
+        <h5 className="text-danger">Todos los campos son obligatorios</h5>
+      ) : null}
+      <h5></h5>
+      <Form onSubmit={submitForm}>
+        <Form.Group>
+          <Form.Label>Nombre del paciente</Form.Label>
+          <Form.Control
+            type="text"
+            name="mascota"
+            placeholder="Nombre del paciente"
+            onChange={handleChangeState}
+            value={mascota}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Nombre del Responsable</Form.Label>
+          <Form.Control
+            type="text"
+            name="padre"
+            placeholder="Nombre del responsable"
+            onChange={handleChangeState}
+            value={padre}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Fecha de cita</Form.Label>
+          <Form.Control
+            type="date"
+            name="fecha"
+            onChange={handleChangeState}
+            value={fecha}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Hora</Form.Label>
+          <Form.Control
+            type="time"
+            name="hora"
+            onChange={handleChangeState}
+            value={hora}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Síntomas/ Descripción</Form.Label>
+          <Form.Control
+            as="textarea"
+            name="sintoma"
+            rows="3"
+            onChange={handleChangeState}
+            value={sintoma}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit" block>
+          Agregar Cita
+        </Button>
+      </Form>
+    </Fragment>
+  );
+};
+
+export default Formulario;
