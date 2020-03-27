e646c9b01f6213f94d7332455a28f97238ef2f32
MarciaGiselle <marcitoledo@alumno.unlam.edu.ar>
2020-03-26 23:09:04 -0300

    Nuevo proyecto Presupuesto

---
+++ Modulo 6/presupuesto/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container} from 'react-bootstrap';
+
+function App() {
+  return (
+    <Container>
+      <header>
+      <h1 className='text-center mt-2'>Gasto Semanal</h1>
+      </header>
+    </Container>
+  );
+}
+
+export default App;
